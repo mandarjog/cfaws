2b9f6d43176abf4e7f2b56b96c85ea098f5ce3e4
Mandar U Jog <mandarjog@gmail.com>
2016-03-17 02:38:49 -0400

    fully tested cleanup deployment, cleans up all resources

--- cleanup_deployment.py
+++ cleanup_deployment.py
@@ -3,6 +3,22 @@ import botocore
 from boto3.session import Session
 
 
+def get_stack_outputvars(stack, ec2):
+    return {v['OutputKey']: v['OutputValue'] for v in stack.outputs}
+
+
+def get_stack(stackName, cff):
+    try:
+        stt = list(cff.stacks.filter(StackName=stackName))
+        return stt[0]
+    except botocore.exceptions.ClientError as e:
+        if "{} does not exist".format(stackName) in\
+                e.response['Error']['Message']:
+            return None
+        else:
+            raise
+
+
 def get_args():
     import argparse
     argp = argparse.ArgumentParser()
@@ -16,8 +32,18 @@ def get_args():
 def main(argv):
     args = get_args().parse_args(argv)
     session = Session(profile_name=args.profile, region_name=args.region)
+    ec2 = session.resource("ec2")
+    s3 = session.resource("s3")
+    cff = session.resource("cloudformation")
     try:
-        cleanup_s3(args.stack_name, session, args.yes)
+        stack = get_stack(args.stack_name, cff)
+        if stack is None:
+            print "Stack in no longer active"
+            return 0
+        stack_vars = get_stack_outputvars(stack, ec2)
+        cleanup_ec2(stack, stack_vars, ec2, args.yes)
+        cleanup_s3(stack, stack_vars, s3, args.yes)
+        stack.delete()
     except botocore.exceptions.NoCredentialsError as ex:
         print ex
         print "Missing ~/.aws/credentials directory?"
@@ -27,27 +53,61 @@ def main(argv):
     return 0
 
 
-def cleanup_s3(stack_name, session, yes):
-    s3 = session.resource("s3")
-    stackbuckets = [_bb for _bb in s3.buckets.filter()
-                    if _bb.name.startswith(stack_name+"-")]
+def cleanup_ec2(stack, stack_vars, ec2, yes):
+    insts = [ii for ii in ec2.instances.filter(
+        Filters=[{'Name': 'vpc-id', 'Values': [stack_vars['PcfVpc']]}])]
+    if confirm_oprn("ec2 instances", len(insts), yes) is False:
+        return -1
 
-    if len(stackbuckets) == 0:
-        print "Nothing to delete"
-        return 0
+    ec2.meta.client.terminate_instances(
+        InstanceIds=[ii.id for ii in insts])
 
-    print "Deleting {} buckets {}".format(len(stackbuckets),
-                                          [b.name for b in stackbuckets])
-    if not yes:
-        confirm = raw_input("This cannot be recovered. Proceed? YES/NO ")
-        if confirm.lower() != 'yes':
-            print "Skipping"
-            return -1
+    def getname(iii):
+        return next(v['Value'] for v in iii.tags if v['Key'] == 'Name')
+
+    for ii in insts:
+        print "Waiting for instance {} to terminate".format(getname(ii))
+        ii.wait_until_terminated()
+    return 0
+
+
+def confirm_oprn(resource_type, resource_number, yes):
+    print "Removing {} {}".format(resource_number, resource_type)
+
+    if resource_number == 0:
+        return False
+    if yes is True:
+        return True
+
+    confirm = raw_input("This cannot be recovered. Proceed? YES/NO ")
+    if confirm.lower() != 'yes':
+        print "Skipping"
+        return False
+
+    return True
+
+
+def cleanup_s3(stack, stack_vars, s3, yes):
+    delkeys = [
+        'PcfOpsManagerS3Bucket', 'PcfElasticRuntimeS3BuildpacksBucket',
+        'PcfElasticRuntimeS3DropletsBucket',
+        'PcfElasticRuntimeS3PackagesBucket',
+        'PcfElasticRuntimeS3ResourcesBucket']
+    bset = set([stack_vars[k] for k in delkeys])
+
+    stackbuckets = [bk for bk in s3.buckets.all() if bk.name in bset]
+
+    if confirm_oprn("s3 buckets", len(stackbuckets), yes) is False:
+        return -1
 
     for bk in stackbuckets:
         print "Deleting {}".format(bk.name)
-        bk.objects.delete()
-        bk.delete()
+        try:
+            bk.objects.delete()
+            bk.delete()
+        except botocore.exceptions.ClientError as ce:
+            if ce.response['Error']['Code'] != '404':
+                print ce
 
     return 0
 
